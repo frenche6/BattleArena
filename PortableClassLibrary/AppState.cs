008b8f2bd7935fd6a2e62337a94150654e65d07f
Thomas French <frenche6_2004@yahoo.com>
2016-02-19 09:29:55 -0500

    Added files via upload

---
+++ PortableClassLibrary/AppState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleArena_PortableClassLibrary
+{
+    public class AppState
+    {
+        public static CharacterModel CharacterModel = new CharacterModel() { PlayerName = "Player" };
+        public static MonsterModel MonsterModel = new MonsterModel() { MonsterName = "Goblin" };
+        public static CombatLog CombatLogList = new CombatLog();
+    }
+}
